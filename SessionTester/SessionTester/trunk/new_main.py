400c1a1afedbcd4c3752c679b41a9569ec039c88
jonathankohl <kohljonathan@gmail.com>
2013-08-12 11:27:36 -0600

    Delete new_main.py
    extraneous file

--- SessionTester/SessionTester/trunk/new_main.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/python
-
-Error reading included file Templates/Python/../Licenses/license-sessiontester.txt
-__author__="Jonathan"
-__date__ ="$9-Nov-2010 11:14:53 AM$"
-
-if __name__ == "__main__":
-    print "Hello World";
